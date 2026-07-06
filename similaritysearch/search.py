02c1582b8a11a30a8e45afd839f16ff0297dbdc4
Lahsaini Yazid <lahsainitv@gmail.com>
2026-07-06 13:19:02 +0100

    Connected to geminillm

--- similaritysearch/search.py
+++ similaritysearch/search.py
@@ -1,13 +1,26 @@
 from sentence_transformers import SentenceTransformer 
+from google import genai 
+import os 
 import faiss 
-import json 
+import json
+key=os.environ.get("key")
+client=genai.Client(api_key=key)
+def charger_prompt(chemin="prompt.txt"):
+    with open(chemin,"r",encoding="utf-8") as f:
+        return f.read()
+prompt=charger_prompt()
+question=input("Entrez votre question:")
 model=SentenceTransformer("all-MiniLM-L6-v2")
 index=faiss.read_index("/home/yazid/rag/vectordb/vector_services.index")
 with open ("/home/yazid/rag/chunks/chunks.json","r",encoding="utf-8") as f:
     chunks=json.load(f)
-question=input("Entrez votre question:")
 query_embedding=model.encode([question])
 k=3
 distances,indices=index.search(query_embedding,k)
-for i in indices[0]:
-    print(chunks[i])
+contexte_recupere="\n\n".join(chunks[i]["content"] for i in indices[0])
+prompt_final=prompt.format(context=contexte_recupere,
+query=question)
+print(prompt_final)
+response=client.models.generate_content(model="gemini-2.5-flash",
+                                        contents=prompt_final)
+print(response.text)
