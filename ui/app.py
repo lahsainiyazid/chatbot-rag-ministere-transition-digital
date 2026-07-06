cc4ebe0e2b75b16ba20b1ffd5251aedb8abd30e8
Lahsaini Yazid <lahsainitv@gmail.com>
2026-07-06 23:17:40 +0100

    Fixed project structure

---
+++ ui/app.py
@@ -0,0 +1,4 @@
+import streamlit as st 
+st.title("Chatbot ministere de la transition digitale:")
+st.logo("/home/yazid/rag/photos/ministere.jpg",size="large")
+st.sidebar.image("/home/yazid/rag/photos/ministere.jpg")
